ae8c9457a816d42dac767d319cfce7dc057d4dd0
Amilcar-B <Amilcar.Barriento@fundacion-jala.org>
2022-05-31 21:30:32 -0300

    add reload button

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import { signInWithGoogle } from "../Firebase";
-import styled, {css} from 'styled-components';
 const Dashboard = () => {
+  function refreshPage() {
+    window.location.reload(false);
+  }
   const[value]=useState(['https://media0.giphy.com/media/l41lZceUiZ3qocBX2/giphy.gif?cid=ecf05e4794gzzzwp38epjeod1huwjzouzi8a279mowcmf2h3&rid=giphy.gif&ct=g'])
   return(
-  
+   
   <div className="App">
-    <button class="login-with-google-btn" onClick={signInWithGoogle}>
+    <button class="login-with-google-btn" onClick={signInWithGoogle}>      
         Sign in with Google
     </button>
+    <button onClick={refreshPage}>Click to reload!</button>
     <div className="title"> Welcome {localStorage.getItem("name")} to Skynet! <img src={localStorage.getItem("profilePic")} /></div>   
     <section>    
       <img src={value} />      
